dee614bc99ca55f00a50676e417b5978fc1633ea
Ramiro Nicolas Funes <127463276+RAdevDS@users.noreply.github.com>
2023-11-14 11:01:35 -0300

    Add files via upload

--- WEBSOCKETS/src/app.js
+++ WEBSOCKETS/src/app.js
@@ -1,44 +1,95 @@
-import express from 'express';
-import { createServer } from 'http';
+import express from 'express'
+import handlebars from 'express-handlebars';
+import { productRouter } from './routes/product.router.js';
+import { ProductManager } from './ProductManager.js'
+import { cartRouter } from './routes/carts.router.js';
+import __dirname from './utils.js';
+import viewRouter from './routes/view.router.js';
 import { Server } from 'socket.io';
-import { fileURLToPath } from 'url';
 import path from 'path';
-import productsRouter from './routes/products.js';
-import cartsRouter from './routes/carts.js';
-import exphbs from 'express-handlebars'; // Importa 'express-handlebars' sin rutas relativas
 
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
+// Definicion de variables
 const app = express();
-const server = createServer(app);
-const io = new Server(server); // Crea un servidor de Socket.io
+const port = 8080;
 
-const PORT = 8080;
-
-const productsFilePath = path.join(__dirname, './data/productos.json');
-const cartsFilePath = path.join(__dirname, './data/carrito.json');
-
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Configura Handlebars como motor de plantillas
-app.engine('handlebars', exphbs());
+app.use('/api/products', productRouter);
+app.use('/api/carts', cartRouter);
+
+app.engine('handlebars', handlebars.engine());
+app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'handlebars');
+app.use(express.static(__dirname + '/public'))
 
+// Ruta del archivo JSON que se utiliza en ProductManager
+const productManagerFilePath = (__dirname + '/Products.json'); 
+const pMI = new ProductManager(productManagerFilePath);
 
-app.use(express.static('public')); // Carpeta para archivos estáticos
-app.use('/api/products', productsRouter(productsFilePath, io));
-app.use('/api/carts', cartsRouter(cartsFilePath));
+app.use('/', viewRouter);
 
-// Ruta para la vista que muestra la lista de productos en tiempo real
-app.get('/realTimeProducts', (req, res) => {
-  const products = []; // Debes obtener los productos de tu base de datos
-
-  // Renderiza la vista 'realTimeProducts' utilizando Handlebars
-  res.render('realTimeProducts', { products });
+const httpServer = app.listen(port, () => {
+    console.log(`Current port ${port}`)
 });
 
-server.listen(PORT, () => {
-  console.log(`Servidor escuchando en el puerto ${PORT}`);
+const io = new Server(httpServer);
+
+io.on('connection', (socket) => {
+    console.log("new client online");
+    socket.on('message', (data) => {
+        // Manejar mensajes si es necesario
+    });
+
+    // Añadir elemento
+    socket.on('add_item', async (data) => {
+        const newProduct = data;
+        const newProductCode = newProduct.code;
+        const array = await pMI.getProducts();
+        console.log("Attempting add new product");
+        const duplicate = array.findIndex(product => product.code === newProductCode);
+        if (duplicate === -1) {
+            if (data.thumbnail.length > 0) {
+                newProduct["thumbnail"] = `${data.thumbnail}`;
+            }
+            pMI.addProduct(newProduct);
+            const updatedArray = await pMI.getProducts();
+            console.log("New product is ");
+            console.log(updatedArray[updatedArray.length - 1]);
+            io.emit("confirm_add", [true, 0, updatedArray[updatedArray.length - 2].id, updatedArray[updatedArray.length - 1]]);
+        } else {
+            console.log(`Error Item ${newProduct.title} by code ${newProductCode} already in database \nSimilar item`);
+            console.log(array[duplicate]);
+            io.emit("confirm_add", [false, array[duplicate], 0, 0]);
+        }
+    });
+
+    // Borrar elemento
+    socket.on('delete', async (data) => {
+        console.log("want to delete ", data);
+        const productId = Number(data);
+        console.log('Product ID to delete: ', productId);
+
+        // Antes de la eliminación
+        console.log('Products before deletion:', await pMI.getProducts());
+
+        const array = await pMI.getProducts();
+        const productIndex = array.findIndex(product => product.id === productId);
+
+        if (productIndex === -1) {
+            console.log(`Item by ID ${productId} was not found`);
+            io.emit('confirm_delete', [false, productId]);
+        } else {
+            pMI.deleteProductById(productId);
+            const updatedArray = await pMI.getProducts();
+
+            // Después de la eliminación
+            console.log('Products after deletion:', updatedArray);
+
+            io.emit('confirm_delete', [true, productId]);
+        }
+    });
+
 });
+
+export default io;
